bb93a563c9244e452c9d052fdcf02e5aab6b503d
jxj322991 <228322991@qq.com>
2020-01-13 19:39:26 +0800

    fix

--- main.js
+++ main.js
@@ -3,24 +3,35 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2020-01-13 11:08:26
- * @LastEditTime : 2020-01-13 14:44:26
+ * @LastEditTime : 2020-01-13 19:39:11
  * @LastEditors  : 靳肖健
  */
 var listArr = []
 
 //工具
 var list_tool = [
-  { url: '', name: '' }
+  // { url: '', name: '' },
+  { url: '//m.jxjweb.top/word_to_html/dist/', name: '自用word转化小工具' },
+  { url: '//m.jxjweb.top/doc_pc/dist/', name: '文档对比器' },
+  { url: '//m.jxjweb.top/form_data/dist/', name: '前端表格辅助系统' }
 ]
 
 //页面
 var list_page = [
+  // { url: '', name: '' },
   { url: '//jxjweb.top/', name: '个人博客' },
-  { url: '//github.com/jxj322991', name: 'github主页' }
+  { url: '//github.com/jxj322991', name: 'github主页' },
+  { url: '//m.jxjweb.top/siwei/dist/', name: '思维导图' },
 ]
 
 //文档
 var list_doc = [
-  { url: '', name: '' }
+  // { url: '', name: '' },
+  { url: '//m.jxjweb.top/doc/egg.pptx', name: '演讲:egg.js在我们项目中的应用' },
+  { url: '//m.jxjweb.top/doc/vue3.pptx', name: 'vue3 核心设计思想' },
+  { url: '//m.jxjweb.top/doc/vue.pptx', name: 'vue 核心介绍' },
+  { url: '//m.jxjweb.top/doc/canvas.pptx', name: '前端绘图技术' },
+  { url: '//m.jxjweb.top/doc/201901star.pptx', name: '2019一季度团队之星' },
+
 ]
 
